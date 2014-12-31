a3cad18d8e44fd98841f5066166bad9600802608
Joe Miller III <joe@devical.com>
2014-12-31 10:21:23 -0800

    Add activate_this.py

---
+++ yourprojectname/venv/bin/activate_this.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+
+import os
+import site
+import sys
+
+# Define Paths
+# base = /path/to/yoursite/venv
+base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if sys.platform == 'win32':
+    site_packages = os.path.join(base, 'Lib', 'site-packages')
+else:
+    site_packages = os.path.join(base, 'lib', 'python{}'.format(sys.version[:3]), 'site-packages')
+
+# Remember original sys.path and os path
+prev_sys_path = list(sys.path)
+prev_os_path = os.environ['PATH']
+
+# Add os path BEFORE old path
+os.environ['PATH'] = base + os.pathsep + os.path.join(base, 'bin') + os.pathsep + prev_os_path
+
+# Add new site-packages directory
+site.addsitedir(site_packages)
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
